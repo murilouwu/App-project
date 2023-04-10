e7aa0675de51ac8880b2312ffd039ce8301ded92
VoidChacal <92901966+VoidChacal@users.noreply.github.com>
2023-04-10 15:54:15 -0300

    Add files via upload

--- assets/java/javaPrinc.js
+++ assets/java/javaPrinc.js
@@ -1,36 +1,3 @@
-//codigos padrões
-/*
-let nuns = [ 
- Math.round((Math.random() * 9 ) + 1), 
- Math.round((Math.random() * 9 ) + 1), 
- Math.round((Math.random() * 9 ) + 1), 
- Math.round((Math.random() * 9 ) + 1), 
- Math.round((Math.random() * 9 ) + 1), 
- Math.round((Math.random() * 9 ) + 1)
-];
-for (var i = 0; i<nuns.length; i++) {
- let lp = 0;
- while(lp==0){
-  for(var i2 = 0; i2<nuns.length; i2++){
-   if(nuns[i] == nuns[i2] && i!=i2){
-    i2 = 0;
-    nuns[i] = Math.round((Math.random() * 9 ) + 1);
-   };
-  }
-  lp=1;
- }
-}
-let poss = [
- Math.round((Math.random() * 5 )), 
- Math.round((Math.random() * 5 )), 
- Math.round((Math.random() * 5 )), 
- Math.round((Math.random() * 5 )), 
- Math.round((Math.random() * 5 )), 
- Math.round((Math.random() * 5 ))
-];
-let res = nuns[poss[0]]+""+nuns[poss[1]]+""+nuns[poss[2]]+""+nuns[poss[3]]+""+nuns[poss[4]]+""+nuns[poss[5]];
-*/
-
 function numberRadom(max, min, qt){
     let nuns = [];
     for(var i=0; i<qt; i++){
@@ -38,22 +5,11 @@ function numberRadom(max, min, qt){
     }
     return nuns;
 };
-
-function numberRadDif(max, min, nuns){
-    for(var i = 0; i<nuns.length; i++) {
-        let lp = 0;
-        while(lp==0){
-            for(var i2 = 0; i2<nuns.length; i2++){
-                if(nuns[i] == nuns[i2] && i!=i2){
-                    i2 = 0;
-                    nuns[i] = Math.round((Math.random() * max ) + min);
-                };
-            }
-            lp=1;
-        };
-    };
-};
-
 function Codigo(digito, val){
-    
+    let poss = [];
+    for(var i=0; i<digito; i++){
+        poss[i] = Math.round((Math.random() *  digito) + 0);
+    }
+    let res = val[poss[0]]+""+val[poss[1]]+""+val[poss[2]]+""+val[poss[3]]+""+val[poss[4]]+""+val[poss[5]];
+    return res;
 }
