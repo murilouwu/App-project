8a1ef2b88c77c8d3f87655fb1d082c5cf6e351d7
VoidChacal <92901966+VoidChacal@users.noreply.github.com>
2023-05-08 15:55:23 -0300

    Add files via upload

--- assets/java/javaPrinc.js
+++ assets/java/javaPrinc.js
@@ -16,3 +16,36 @@ function Codigo(digito, val){
     }
     return res;
 }
+
+function PreViewImg(input, imgPreview){
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $(imgPreview).attr('style', "background-image: url('"+e.target.result+"');");
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+function subir(){
+	window.scrollTo(0, 0);
+}
+
+function mostrar(ocu, chave){
+    for (var i=0; i<ocu.length; i++){	
+		if (i<chave){
+			ocultar(ocu[i], 0);	
+		}else{
+			ocultar(ocu[i], 1);
+		};
+	};
+};
+
+function ocultar(obj, es){
+	let div = document.querySelector(obj);
+	if(es==1){
+		div.style.display = 'flex';
+	}else{
+		div.style.display = 'none';
+	};
+};
