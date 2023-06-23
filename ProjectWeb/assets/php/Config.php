ffc25f7e3ff2e5289e4192b37f8d4d3c3f1f06e0
Murilo Gimenez <91744500+murilouwu@users.noreply.github.com>
2023-06-23 10:06:41 -0300

    Add files via upload

--- ProjectWeb/assets/php/Config.php
+++ ProjectWeb/assets/php/Config.php
@@ -9,61 +9,88 @@ $db = array(
     'nm'=>'CRONOMETRAIS'
 );
 
-$conn = mysqli_connect($db['host'], $db['user'], $db['pass'], $db['nm']) or die ('Sem Conecção ao database');
+//$conn = mysqli_connect($db['host'], $db['user'], $db['pass'], $db['nm']) or die ('Sem Conecção ao database');
 
 function InsertBd($Infs, $Dts, $Vls, $Vers, $Configs, $Up){
     /*
         $Infs = array('nm_table', );
-        $Dts = array('date1', 'date2', ...);
-        $Vls = array('insert_value1','insert_value2'...);
+        $Dts = array('date1', 'date2', ...); 
+        $Vls = array('insert_value1','insert_value2'...); if isset(file){ $Vls = array('insert_value1', 'insert_value2', ...['fileName0','fileName1','fileName2',...]); }
         $Vers = array(0, 1, 2..); key from var Dts
         $Configs = array(array('OR ou AND 0', 'OR ou AND 1'...), 'ERROR'=>array('erro1', 'erro2', ..), 'FilesPast'=>array('FilePastSave0','FilePastSave1', ...));
         $Up = array($file0, $file1...);
     */
     $VersToText = '';
-    for ($i=0; $i<count($Vers); $i++){
-        if($i==0){
-            $VersToText = $VersToText.$Dts[$Vers[$i]].'= '.$Vls[$Vers[$i]];
-        }else{
-            $VersToText = $VersToText.' '.$Configs[0][($i-1)].' '.$Dts[$Vers[$i]].'= '.$Vls[$Vers[$i]];
+    foreach ($Vers as $key => $ver) {
+        if ($key === 0) {
+            $VersToText .= $Dts[$ver] . '= ' . $Vls[$ver];
+        } else {
+            $VersToText .= ' ' . $Configs[0][$key - 1] . ' ' . $Dts[$ver] . '= ' . $Vls[$ver];
         }
     }
-    $query = 'SELECT * FROM '.$Infs[0].' WHERE '.$VersToText;
+    $query = 'SELECT * FROM ' . $Infs[0] . ' WHERE ' . $VersToText;
     $res = $GLOBALS['conn']->query($query);
     $rows = mysqli_num_rows($res);
 
-    if($rows > 0){
+    if ($rows > 0) {
         mensage($Configs['ERROR'][0]);
-    }else if($rows == 0){
-        if(count($Up) > 0){
+    } else if ($rows == 0) {
+        if (count($Up) > 0) {
             $NewLinkFiles = upFileinPage($Up, $Configs['FilesPast']);
-            //chatgpt help me
+            $Vls[count($Vls) - 1] = $NewLinkFiles;
         }
-        $DtsToText = '';
-        for ($i=0; $i<count($Dts); $i++){
-            if($i==0){
-                $DtsToText = $DtsToText.$Dts[$i];
-            }else{
-                $DtsToText = $DtsToText.', '.$Dts[$i];
-            }
-        }
+        $DtsToText = implode(', ', $Dts);
         $VlsToText = '';
-        for ($i=0; $i<count($Vls); $i++){
-            if($i==0){
-                $VlsToText = $VlsToText.$Vls[$i];
-            }else{
-                $VlsToText = $VlsToText.', '.$Vls[$i];
+        foreach ($Vls as $key => $vl) {
+            if ($key === (count($Vls) - 1)) {
+                $VlsToText .= implode(', ', $vl);
+            } else {
+                $VlsToText .= $vl;
+            }
+            if ($key !== (count($Vls) - 1)) {
+                $VlsToText .= ', ';
             }
         }
 
-        $query = 'INSERT INTO '.$Infs[0].' ( '.$DtsToText.' ) VALUES ( '.$VlsToText.' )';
+        $query = 'INSERT INTO ' . $Infs[0] . ' ( ' . $DtsToText . ' ) VALUES ( ' . $VlsToText . ' )';
         $res = $GLOBALS['conn']->query($query);
-        if($res){
-            //return GetBd($Infs, $Dts, $Vls);
-        }else{
+        if ($res) {
+            return GetBd($Infs, $Dts, $Vls);
+        } else {
             mensage($Configs['ERROR'][1]);
         }
     }
 }
+
+function GetBd($Infs, $Dts, $Vls) {
+    $query = 'SELECT ';
+
+    if (empty($Dts)) {
+        $query .= '* ';
+    } else {
+        $query .= implode(', ', $Dts) . ' ';
+    }
+
+    $query .= 'FROM ' . $Infs[0] . ' WHERE ';
+    
+    $conditions = array();
+    foreach ($Dts as $dt) {
+        $conditions[] = $dt . " = '" . $Vls[$dt] . "'";
+    }
+    
+    $query .= implode(' AND ', $conditions);
+    $res = $GLOBALS['conn']->query($query);
+    
+    if($res){
+        $result = array();
+
+        while ($row = mysqli_fetch_assoc($res)) {
+            $result[] = $row;
+        }
+        
+        return $result;
+    }
+}
+
 
 ?>
--- ProjectWeb/assets/php/PhpMain.php
+++ ProjectWeb/assets/php/PhpMain.php
@@ -1,34 +1,71 @@
 <?php
-function HeaderEcho($Title, $assets){
+function HeaderEcho($Title, $assets, $itemPlus) {
     $res = '
         <!DOCTYPE html>
-            <html>
-            <head>
-                <meta charset="utf-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1">
-                <script src="https://kit.fontawesome.com/39cab4bf95.js" crossorigin="anonymous"></script>
-                <script src="https://code.jquery.com/jquery-3.2.1.slim.js" integrity="sha256-tA8y0XqiwnpwmOIl3SGAcFl2RvxHjA8qp0+1uCGmRmg=" crossorigin="anonymous"></script>
-                <link rel="stylesheet" type="text/css" href="'.$assets[0].'">
-                <script src="'.$assets[1].'"></script>
-                <link rel="shortcut icon" href="'.$assets[2].'">
-                <title>'.$Title.'</title>
-            </head>
+        <html>
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+    ';
+    if (isset($assets[0]) && is_array($assets[0])) {
+        foreach ($assets[0] as $cssLink) {
+            $res .= '<link rel="stylesheet" type="text/css" href="' . $cssLink . '"';
+            if (isset($itemPlus[0]) && is_array($itemPlus[0]) && $itemPlus[0][0] == 0) {
+                $res .= ' ' . $itemPlus[0][2];
+            }
+            $res .= '>';
+        }
+    }
+    if (isset($assets[1]) && is_array($assets[1])) {
+        foreach ($assets[1] as $jsLink) {
+            $res .= '<script src="' . $jsLink . '"';
+            if (isset($itemPlus[1]) && is_array($itemPlus[1]) && $itemPlus[1][0] == 0) {
+                $res .= ' ' . $itemPlus[1][2];
+            }
+            $res .= '></script>';
+        }
+    }
+
+    $res .= '
+            <link rel="shortcut icon" href="'.$assets[2].'">
+            <title>'.$Title.'</title>
+        </head>
     ';
     echo($res);
 }
+
 
 function mensage($txt){
     echo '<script>alert("'.$txt.'");</script>';
 }
 
-function upFileinPage($Files, $PastSaves){
-    for($i=0; $i<count($PastSaves); $i++){
-        $directory = dirname(__DIR__) . '/'.$PastSaves[$i];
-        if(!is_dir($directory)){
+function upFileinPage($Files, $PastSaves) {
+    $Ret = array();
+    
+    for ($i = 0; $i < count($PastSaves); $i++) {
+        $directory = dirname(__DIR__) . '/' . $PastSaves[$i];
+        
+        if (!is_dir($directory)) {
             mkdir($directory, 0777, true);
         }
+        
+        $file = $Files[$i];
+        $fileName = $file['name'];
+        $fileSize = $file['size'];
+        $fileDate = date('YmdHis');
+        
+        $newFileName = $fileName . $fileSize . $fileDate;
+        $destination = $directory . '/' . $newFileName;
+        
+        move_uploaded_file($file['tmp_name'], $destination);
+        
+        $Ret[] = $destination;
     }
+    
+    return $Ret;
 }
+
+
 
 function footEcho(){
     $res = '
