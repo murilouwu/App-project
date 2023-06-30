f0bca752825152f27cce071260d18e740396a78b
Murilo Gimenez <91744500+murilouwu@users.noreply.github.com>
2023-06-30 12:26:36 -0300

    Update PhpMain.php

--- ProjectWeb/assets/php/PhpMain.php
+++ ProjectWeb/assets/php/PhpMain.php
@@ -1,38 +1,44 @@
 <?php
 function HeaderEcho($Title, $assets, $itemPlus) {
-    $res = '
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-    ';
-    if (isset($assets[0]) && is_array($assets[0])) {
-        foreach ($assets[0] as $cssLink) {
-            $res .= '<link rel="stylesheet" type="text/css" href="' . $cssLink . '"';
-            if (isset($itemPlus[0]) && is_array($itemPlus[0]) && $itemPlus[0][0] == 0) {
-                $res .= ' ' . $itemPlus[0][2];
-            }
-            $res .= '>';
-        }
-    }
-    if (isset($assets[1]) && is_array($assets[1])) {
-        foreach ($assets[1] as $jsLink) {
-            $res .= '<script src="' . $jsLink . '"';
-            if (isset($itemPlus[1]) && is_array($itemPlus[1]) && $itemPlus[1][0] == 0) {
-                $res .= ' ' . $itemPlus[1][2];
-            }
-            $res .= '></script>';
-        }
-    }
+		$res = '
+			<!DOCTYPE html>
+			<html>
+			<head>
+		';
+	
+		if (is_array($assets)) {
+			foreach ($assets as $asset) {
+				$type = $asset[0];
+				$link = $asset[1];
+				$extra = isset($asset[2]) ? $asset[2] : null;
+	
+				if ($type == 0) {
+					$res .= '<meta ' . $link . '>';
+				} elseif ($type == 1) {
+					$res .= '<link rel="stylesheet" type="text/css" href="' . $link . '"';
+					if ($extra !== null) {
+						$res .= ' ' . $extra;
+					}
+					$res .= '>';
+				} elseif ($type == 2) {
+					$res .= '<script src="' . $link . '"';
+					if ($extra !== null) {
+						$res .= ' ' . $extra;
+					}
+					$res .= '></script>';
+				}
+			}
+		}
+	
+		$res .= '
+				<link rel="shortcut icon" href="'.$itemPlus.'">
+				<title>'.$Title.'</title>
+			</head>
+		';
+	
+		echo $res;
+	}
 
-    $res .= '
-            <link rel="shortcut icon" href="'.$assets[2].'">
-            <title>'.$Title.'</title>
-        </head>
-    ';
-    echo($res);
-}
 
 
 function mensage($txt){
